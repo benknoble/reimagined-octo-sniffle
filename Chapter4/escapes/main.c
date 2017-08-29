ab004273c479c03ca728e7eac8b488bc70ea84c7
David Ben Knoble <ben.knoble@gmail.com>
2017-08-29 17:29:00 -0400

    Add program demonstrating escapes 4.1

---
+++ Chapter4/escapes/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Column A\tColumn B\tColumn C");
+    printf("\nMy Computer\'s beep sounds like this: \a!\n");
+    printf("\"Letz\b\'s fix that typo and then show the backslash ");
+    printf("character \\,\" she said\n");
+}
